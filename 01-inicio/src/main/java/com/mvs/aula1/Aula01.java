9b8a45329026a78c2f5af273fe81918c1410c22e
Marcos <marvinred@gmail.com>
2020-01-12 00:38:03 -0200

    [Aula 1 springboot]

---
+++ 01-inicio/src/main/java/com/mvs/aula1/Aula01.java
@@ -0,0 +1,14 @@
+package com.mvs.aula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01.class, args);
+
+	}
+
+}
